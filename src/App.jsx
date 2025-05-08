0d491f8a6cf2886f613952c34851d59c7a547320
Lindsey <lawsonlindsey36@yahoo.com>
2025-05-08 10:25:58 -0400

    debugged

--- src/App.jsx
+++ src/App.jsx
@@ -138,5 +138,4 @@ function App() {
   );
 }
 
-
 export default App
--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
-        start_url: '/',
+        start_url: '/StitchTally/',
         icons: [
           {
             src: '/icons/icon-192.png',
